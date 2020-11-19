bd78b14bf4a763e38c23c72c608572ddb770ab9f
Annika Suu <annika.suu@khk.ee>
2020-11-19 09:44:21 +0200

    eesnime ja perenime pidi tervitamine

---
+++ praks3/p3yl1.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+#Antud skript küsib kasutaja ees- ja perenime ning tervitab teda.
+
+echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+echo "Tere, $eesnimi $perenimi!"
+
+echo "Sinu perenimi on $perenimi ja eesnimi on $eesnimi."
