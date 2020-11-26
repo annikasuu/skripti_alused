1ec55eb5e58d3799b3ede88f77641d6b52b28e71
Annika Suu <annika.suu@khk.ee>
2020-11-26 14:24:48 +0200

    nested if ja veateade arvu mitte andmisel

--- praks4/paarsus.sh
+++ praks4/paarsus.sh
@@ -4,7 +4,10 @@
 #Seepeale antakse vastus, kas antud arv on paaris või mitte.
 #Kui algselt arvu ei sisstata, siis annab skript seletava teate.
 
-echo 
+echo
+if [ $# -eq 0 ]; then
+echo "Palun sisesta suvaline täisarv." 
+else
 #Kontrollimaks, kas tegemist on täisarvuga, jagame kasutaja sisestatud arvu 2-ga.
 #Arvutame jäägi 2-ga jagamisel. $1 tähistab sisestatud muutujat.
 jaak=$(($1 % 2))
@@ -12,16 +15,10 @@ jaak=$(($1 % 2))
 #4 % 2 => 0 -> siis arv on paaris
 #5 % 2 => 1 -> siis arv on paaritu
 
-if [ $jaak -eq 0 ]
-then
-  echo "Antud arv on paaris."
-else
-   if [ $jaak -eq 1 ]
-   then
-      echo "Antud arv on paaritu."
-   else 
-      echo "Palun sisesta skripti käivitamisel suvaline täisarv."
-   fi
+  if [ $jaak -eq 0 ]; then
+   echo "Antud arv on paaris."
+  else
+   echo "Antud arv on paaritu."
+  fi
 fi
-#Tingimuskontroll on lõppenud. 
- 
+#Tingimuskontroll on lõppenud.
